45bfa101b15b3b3913bc0bdffbaf5250231bc29b
ChiChaSsSSs <aantic112@gmail.com>
2024-12-12 19:08:59 +0100

    Delete Projekat/Frontend directory

--- Projekat/Frontend/app/main.js
+++
@@ -1,6 +0,0 @@
-import { Aplikacija } from "./aplikacija.js";
-
-const prodavnice = await fetch("https://localhost:7080/Ispit/VratiProdavnice").then(response => response.json());
-const aplikacija = new Aplikacija(prodavnice);
-aplikacija.pokreniAplikaciju(document.body);
-
